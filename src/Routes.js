2b875577292da7071049eddb378947faddfcf622
youngho052 <qkrdudgh2701@gmail.com>
2021-02-04 00:58:55 +0900

    Add: 고수 프로필 레이아웃 구현
    Modify: 고수프로필 목데이터로 구현
    
    Modify: Data파일 생성
    
    Modify : 고수프로필 데이터 연동 성공
    
    Modify: 데이터 연동 수정

--- src/Routes.js
+++ src/Routes.js
@@ -10,21 +10,31 @@ import Main from "./Pages/Main/Main";
 import RequestPage from "./Pages/RequestPage/RequestPage";
 import Footer from "./Components/Footer/Footer";
 import RequestDetail from "./Pages/RequestDetail/RequestDetail";
+import SearchDetail from "./Pages/SearchDetail/SearchDetail";
 
 function Routes() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Main} />
+<<<<<<< HEAD
         <Route exact path="/:category" component={Main} />
         <Route exact path="/signup" component={Signup} />
+=======
+        <Route exact path="/category" component={Main} />
+        {/* <Route exact path="/signup" component={Signup} /> */}
+>>>>>>> 7beed02 (Add: 고수 프로필 레이아웃 구현)
         <Route exact path="/login" component={Login} />
         <Route exact path="/requestpage" component={RequestPage} />
         <Route exact path="/footer" component={Footer} />
         <Route exact path="/requestpage/detail" component={RequestDetail} />
+<<<<<<< HEAD
         <Route exact path="/find-password" component={FindPassword} />
         <Route exact path="/find-password/send" component={Send} />
         <Route exact path="/reset/:token" component={Reset} />
+=======
+        <Route exact path="/search/detail" component={SearchDetail} />
+>>>>>>> 7beed02 (Add: 고수 프로필 레이아웃 구현)
       </Switch>
     </Router>
   );
